582031203a471d1650028f67922de269d3a5809f
YJK <dhdlstjtr@gmail.com>
2024-10-12 14:17:14 +0900

    kernel/tty: Remove deprecated logging code

--- include/kernel/io/tty.h
+++ include/kernel/io/tty.h
@@ -16,18 +16,4 @@ void tty_putc(char c);
 void tty_puts(char const *s);
 void tty_vprintf(char const *fmt, va_list ap);
 char tty_getchar(void);
-
-typedef enum LogLevel {
-    LOGLEVEL_INFO  = 'I',
-    LOGLEVEL_ERROR = 'E',
-    LOGLEVEL_DEBUG = 'D',
-} LogLevel;
-
 void tty_printf(char const *format, ...);
-
-#define Log(_lv, ...)    tty_printf("[LOG]"); tty_printf(__VA_ARGS__); tty_printf("\n");
-
-#define CON_LOGI(...)           Log(LOGLEVEL_INFO, __VA_ARGS__)
-#define CON_LOGE(...)           Log(LOGLEVEL_ERROR, __VA_ARGS__)
-#define CON_LOGD(...)           Log(LOGLEVEL_DEBUG, __VA_ARGS__)
-
