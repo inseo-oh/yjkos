8bcbc01435d682d2596631355bf6052e50fe1515
YJK <dhdlstjtr@gmail.com>
2024-10-20 19:24:37 +0900

    shell/test: Code cleanup

--- kernel/shell/test/test.c
+++ kernel/shell/test/test.c
@@ -1,6 +1,9 @@
 #include "test.h"
+#include <kernel/io/tty.h>
 
-bool test_expect_impl(bool b, char const *expr, char const *func, char const *file, int line) {
+bool test_expect_impl(
+    bool b, char const *expr, char const *func, char const *file, int line)
+{
     if (!b) {
         tty_printf("test failed in %s(%s:%d) - failed test expresion: %s\n", func, file, line, expr);
     }
--- kernel/shell/test/test.h
+++ kernel/shell/test/test.h
@@ -13,10 +13,15 @@ struct testgroup {
     size_t testslen;
 };
 
-bool test_expect_impl(bool b, char const *expr, char const *func, char const *file, int line);
+bool test_expect_impl(
+    bool b, char const *expr, char const *func, char const *file, int line);
 
-#define TEST_EXPECT(_x)         if (!test_expect_impl(_x, #_x, __func__, __FILE__, __LINE__)) { return false; }
-#define TEST_RUN(_name, _tests) test_run_impl(_name, (_tests), sizeof(_tests)/sizeof(*(_tests)))
+#define TEST_EXPECT(_x)                                             \
+    if (!test_expect_impl(_x, #_x, __func__, __FILE__, __LINE__)) { \
+        return false;                                               \
+    }
+#define TEST_RUN(_name, _tests) \
+    test_run_impl(_name, (_tests), sizeof(_tests)/sizeof(*(_tests)))
 
 // lib
 extern const struct testgroup TESTGROUP_BITMAP;
