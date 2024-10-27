1491a4dab717a9257149fc696c92bc4ae8f23fe6
YJK <dhdlstjtr@gmail.com>
2024-10-27 15:40:47 +0900

    disable interrupts when pancking

--- kernel/panic.c
+++ kernel/panic.c
@@ -1,14 +1,18 @@
 #include <kernel/arch/hcf.h>
+#include <kernel/arch/interrupts.h>
+#include <kernel/arch/stacktrace.h>
 #include <kernel/io/tty.h>
 #include <kernel/lib/noreturn.h>
 #include <kernel/panic.h>
 #include <stddef.h>
 
 NORETURN void panic(char const *msg) {
+    arch_interrupts_disable();
     tty_puts("\nFATAL SOFTWARE FAILURE -- SYSTEM NEEDS TO RESTART.\n");
     if (msg != NULL) {
         tty_puts(msg);
         tty_putc('\n');
     }
+    arch_stacktrace();
     arch_hcf();
 }
